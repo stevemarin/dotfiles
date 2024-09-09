9101e7eeea9d6404730acae79c321fab5e92c649
Steve Marin <steve.marin@gmail.com>
2024-09-09 08:48:04 -0500

    cleanup

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
-sudo pacman -Sy stow cmake neovide lazygit luarocks difftastic fd just ripgrep \
-                hurl bottom starship eza jql gnome-terminal pass gnupg
+#!/bin/bash
+
+sudo pacman -Sy --needed stow cmake neovide lazygit luarocks difftastic fd just ripgrep \
+                hurl bottom starship eza gnome-terminal pass gnupg shellcheck
 
 # install lazyvim
 if [ ! -e ~/.config/nvim/lua/config/lazy.lua ]; then
@@ -9,15 +11,17 @@ fi
 # install rust
 if ! command -v cargo; then
   sudo curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+  cargo install cargo-binstall
+  cargo binstall --no-confirm jql
 fi
 
 # install bash-preexec for atuin
 if [ ! -e ~/.bash-preexec.sh ]; then
   curl https://raw.githubusercontent.com/rcaloras/bash-preexec/master/bash-preexec.sh -o ~/.bash-preexec.sh
-figit push --set-upstream origin main
+fi
 
 # install atuin
-if ! command atuin; then
+if ! command -v atuin; then
   bash <(curl --proto '=https' --tlsv1.2 -sSf https://setup.atuin.sh)
 fi
 
@@ -40,14 +44,33 @@ fi
 FONT_DIR="/usr/local/share/fonts"
 sudo mkdir --parents ${FONT_DIR}
 DIR="$(mktemp -d)"
-pushd "${DIR}"
-wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/FiraCode.zip
-wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/Hack.zip
-wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/Inconsolata.zip
-wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/JetBrainsMono.zip
-wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/Ubuntu.zip
-find . -name "*.zip" -exec unzip -n {} \;
-find . -name "*.zip" -exec rm {} \;
-sudo mv *.ttf ${FONT_DIR}
-sudo fc-cache -fv
-popd
+pushd "${DIR}" || exit 1
+
+if ! compgen "FiraCodeNerdFont*.ttf" > /dev/null; then
+  wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/FiraCode.zip
+fi
+
+if ! compgen "HackNerdFont*.tft" > /dev/null; then
+  wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/Hack.zip
+fi
+
+if ! compgen "InconsolataNerdFont*.ttf" > /dev/null; then
+  wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/Inconsolata.zip
+fi
+
+if ! compgen "JetBrainsMonoNerdFont*.ttf" > /dev/null; then
+  wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/JetBrainsMono.zip
+fi
+
+if ! compgen "UbuntuNerdFont*.ttf" > /dev/null; then
+  wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/Ubuntu.zip
+fi
+
+if ! compgen "*.zip" > /dev/null; then
+  find . -name "*.zip" -exec unzip -n {} \;
+  find . -name "*.zip" -exec rm {} \;
+  sudo mv ./*.ttf* ${FONT_DIR}
+  sudo fc-cache -fv
+fi
+
+popd || exit 1
